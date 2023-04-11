658fd0b513fdded0d7e3374e2b5ba598051056bd
ogemboeugene <129948903+ogemboeugene@users.noreply.github.com>
2023-04-11 12:50:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,40 @@
-/**menu icon navbar */
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// Header toggle
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bxs-x-circle');
-    navbar.classList.toggle('active');
-};
+let MenuBtn = document.getElementById('MenuBtn')
 
+MenuBtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
 
-/**scroll section active link */
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+// Typing Effects
 
-window.onscroll = () =>{
-    sections.forEach(sec =>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+let typed = new Typed('.auto-input',{
+    strings: ['Front-End Developer!','Freelancer!','Game developer','UI Designer!','YouTuber!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+// Activve link state on scroll
+
+// Get all links
+let navLinks = document.querySelectorAll('nav ul li a')
+
+// Get All Sections
+let sections = document.querySelectorAll('section')
+
+window.addEventListenetr('scroll', function (){
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
             });
-        };
+        }
     });
-
-
-/**sticky navbar */
-    let header = document.querySelector('.header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-
-
-
-    /**remove menu when click(scroll) */
-    menuIcon.classList.remove('bxs-x-circle');
-    navbar.classList.remove('active');
-
-    };
-
-/**remove menu when click(scroll) */
-    menuIcon.classList.remove('bxs-x-circle');
-    navbar.classList.remove('active');
-
-
- /**scroll reveal */
-    ScrollReveal({ 
-        reset: true,
-        distance: '80px',
-        duration: 2000,
-        delay: 200
-    });
-
-    ScrollReveal().reveal('.home-content, .heading', { 
-        origin: 'top' 
-    });
-
-     ScrollReveal().reveal('.home-img img, .service, .contact form, .container, .about-content', { 
-        origin: 'bottom' 
-    });
-     ScrollReveal().reveal('.home-content h1, .about-img img,.home-content ul', { 
-        origin: 'left' 
-    });
-     ScrollReveal().reveal('.home-content h3, .home-content p, .about-content h3,.giphy-embed', { 
-        origin: 'right' 
-    });
-
+});
